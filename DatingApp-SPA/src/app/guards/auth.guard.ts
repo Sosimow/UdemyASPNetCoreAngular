c5dcf3fbc0095e71da24f8ab546a2c3abd3435f8
Florian Stegemann <fstegemann87@googlemail.com>
2020-06-04 15:45:38 +0200

    Add Angular routing

---
+++ DatingApp-SPA/src/app/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { AlertifyService } from '../services/alertify.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private alertify: AlertifyService
+  ) {}
+
+  canActivate(): boolean {
+    if(this.authService.loggedIn()) {
+      return true;
+    }
+
+    this.alertify.error('You need to be logged in to access this page.');
+    this.router.navigate(['/home']);
+  }
+}
